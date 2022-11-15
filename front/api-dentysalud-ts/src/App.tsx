f3f8b1726aa5e6b2298eee6f14e5d93858878a91
AlexisYovera <alexisyovera29@gmail.com>
2022-11-15 12:05:18 -0500

    CreacionProyectoReact

---
+++ front/api-dentysalud-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//ESTE ES EL COMPONENTE APP
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
